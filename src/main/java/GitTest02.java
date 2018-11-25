c44ac3a0d5efa475ae2ff139b23537632a709bb3
yupang <491278210@qq.com>
2018-11-25 19:14:48 +0800

    验证2

--- src/main/java/GitTest02.java
+++ src/main/java/GitTest02.java
@@ -3,4 +3,10 @@ public class GitTest02 {
     public static void main(String[] args) {
         System.out.println("git2");
     }
+
+    public static void test(){
+        System.out.println(
+                "测试2"
+        );
+    }
 }
